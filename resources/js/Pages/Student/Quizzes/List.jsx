b44f3ac6cb1a61f754ad4b6ba1c09a094af0116e
asaber3030 <abdulrahmansaber70@gmail.com>
2024-12-11 06:02:35 +0200

    fix welcome

--- resources/js/Pages/Student/Quizzes/List.jsx
+++ resources/js/Pages/Student/Quizzes/List.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import '@/assets/css/students/global-students.scss'
-import './quizzes.scss'
+import "@/assets/css/students/global-students.scss";
+import "./quizzes.scss";
 
-import {Head, Link, usePage} from "@inertiajs/inertia-react";
+import { Head, Link, usePage } from "@inertiajs/inertia-react";
 
 import StudentLayout from "@/Pages/Student/Layout/Layout";
 import PageHeader from "@/components/PageHeader/PageHeader";
@@ -12,50 +12,58 @@ import formatDate from "@/helpers/functions/format-date";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Tooltip } from "@mui/material";
 import { SelectBox } from "@/components/Form/FormContainer";
-import {faEye, faPlay, faQuestionCircle, faSearch} from "@fortawesome/free-solid-svg-icons";
+import {
+  faEye,
+  faPlay,
+  faQuestionCircle,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 import { fullDateOptions } from "@/helpers/constants";
 import { QUIZ_STATUS_OBJECT } from "@/helpers/constants";
 import NotFoundAlert from "@/components/NotFoundAlert";
 import Btn from "@/components/Btn";
-import {Inertia} from "@inertiajs/inertia";
+import { Inertia } from "@inertiajs/inertia";
 
 const ListQuizzes = () => {
+  const { appURL, user, quizzes, subjects } = usePage().props;
 
-  const { appURL, user, quizzes, subjects } = usePage().props
-
-  const [data, setData] = useState(quizzes)
-  const [sub, setSub] = useState(null)
+  const [data, setData] = useState(quizzes);
+  const [sub, setSub] = useState(null);
 
   const applySearch = () => {
-    Inertia.get(route('students.quizzes.view.subject', sub))
-  }
+    Inertia.get(route("students.quizzes.view.subject", sub));
+  };
 
   useEffect(() => {
     if (sub) {
-      setData(quizzes.filter(q => q.subject.id == sub))
+      setData(quizzes.filter((q) => q.subject.id == sub));
     }
-  }, [sub])
+  }, [sub]);
 
   return (
     <StudentLayout user={user}>
-
-      <Head
-        title='Quizzes'
-      />
+      <Head title="Quizzes" />
 
       <PageHeader
-        pageTitle={'Quizzes'}
+        pageTitle={"Quizzes"}
         pageIcon={faQuestionCircle}
-        pageRightComponent={<span className='text-dark fw-bold'>{user.year.title} / {user.year.term_name}</span>}
+        pageRightComponent={
+          <span className="text-dark fw-bold">
+            {user.year.title} / {user.year.term_name}
+          </span>
+        }
       />
 
       <div className="list-quizzes">
-        {subjects.map(subject => (
-          <Link href={route('students.quizzes.view.subject', subject.id)}>
-            <img src={appURL + subject.icon} alt=""/>
+        {subjects.map((subject) => (
+          <Link href={route("students.quizzes.view.subject", subject.id)}>
+            <img src={appURL + subject.icon} alt="" />
             <span>
               <span>{subject.title}</span>
-              <span className='gray-span'>{subject.quizzes.length} quiz{subject.quizzes.length > 1 ? 'zes' : ''}</span>
+              <span className="gray-span">
+                {subject.quizzes.length} quiz
+                {subject.quizzes.length > 1 ? "zes" : ""}
+              </span>
             </span>
           </Link>
         ))}
@@ -64,79 +72,100 @@ const ListQuizzes = () => {
       <div className="list-quizzes-tbl">
         <div className="filter-quizzes">
           <h6 style={{ fontSize: 17 }}>All Quizzes</h6>
-          <div className='search-handler'>
+          <div className="search-handler">
             <SelectBox
-              items={subjects.map(s => {
-                return { text: `${s.title} (${s.code})`, value: s.id }
+              items={subjects.map((s) => {
+                return { text: `${s.title} (${s.code})`, value: s.id };
               })}
-              handleChange={ e => setSub(e.target.value) }
+              handleChange={(e) => setSub(e.target.value)}
             />
-            <Btn disabled={sub ? false : true } handleClick={applySearch}><FontAwesomeIcon icon={faSearch} /></Btn>
+            <Btn disabled={sub ? false : true} handleClick={applySearch}>
+              <FontAwesomeIcon icon={faSearch} />
+            </Btn>
           </div>
-
         </div>
         {data.length > 0 ? (
           <table className="table">
             <thead>
-            <tr>
-              <th scope="col">Subject</th>
-              <th scope="col">Status</th>
-              <th scope="col">Quiz Grade</th>
-              <th scope="col">Start</th>
-              <th scope="col">End</th>
-              <th scope="col">Minutes</th>
-              <th scope="col">Created</th>
-              <th scope="col">Actions</th>
-            </tr>
+              <tr>
+                <th scope="col">Subject</th>
+                <th scope="col">Status</th>
+                <th scope="col">Quiz Grade</th>
+                <th scope="col">Start</th>
+                <th scope="col">End</th>
+                <th scope="col">Minutes</th>
+                <th scope="col">Created</th>
+                <th scope="col">Actions</th>
+              </tr>
             </thead>
             <tbody>
-            {data.map(quiz => {
+              {data.map((quiz) => {
+                const startDate = new Date(quiz.start);
+                const minutes = quiz.minutes + startDate.getMinutes();
 
-              const startDate =  new Date(quiz.start)
-              const minutes = quiz.minutes + startDate.getMinutes()
+                startDate.setMinutes(minutes);
 
-              startDate.setMinutes(minutes)
+                const now = new Date();
 
-              const now = new Date()
-
-              return (
-                <tr key={quiz.id}>
-                  <td><Link href={route('students.subjects.view', quiz.subject.id)}>{quiz.subject.title}</Link></td>
-                  {quiz.status == 0 && (
-                    <td className='text-danger'>{QUIZ_STATUS_OBJECT[quiz.status]}</td>
-                  )}
-                  {(quiz.status == 1 || quiz.status == 2) && (
-                    <td className='text-success'>{QUIZ_STATUS_OBJECT[quiz.status]}</td>
-                  )}
-                  <td>{quiz.grade} mark(s)</td>
-                  <td>{formatDate(quiz.start, fullDateOptions)}</td>
-                  <td>{formatDate(quiz.end, fullDateOptions)}</td>
-                  <td>{quiz.minutes} minute(s)</td>
-                  <td>{formatDate(quiz.created_at)}</td>
-                  <td>
-                    <div className="table-actions">
-                      {new Date() <= new Date(quiz.end) && (
-                        <Tooltip title='Join'>
-                          <Link href={route('students.quizzes.join', quiz.id)} className='btn btn-sm btn-dark'><FontAwesomeIcon icon={faPlay} /></Link>
+                return (
+                  <tr key={quiz.id}>
+                    <td>
+                      <Link
+                        href={route("students.subjects.view", quiz.subject.id)}
+                      >
+                        {quiz.subject.title}
+                      </Link>
+                    </td>
+                    {quiz.status == 0 && (
+                      <td className="text-danger">
+                        {QUIZ_STATUS_OBJECT[quiz.status]}
+                      </td>
+                    )}
+                    {(quiz.status == 1 || quiz.status == 2) && (
+                      <td className="text-success">
+                        {QUIZ_STATUS_OBJECT[quiz.status]}
+                      </td>
+                    )}
+                    <td>{quiz.grade} mark(s)</td>
+                    <td>{formatDate(quiz.start, fullDateOptions)}</td>
+                    <td>{formatDate(quiz.end, fullDateOptions)}</td>
+                    <td>{quiz.minutes} minute(s)</td>
+                    <td>{formatDate(quiz.created_at)}</td>
+                    <td>
+                      <div className="table-actions">
+                        {new Date() <= new Date(quiz.end) && (
+                          <Tooltip title="Join">
+                            <Link
+                              href={route("students.quizzes.join", quiz.id)}
+                              className="btn btn-sm btn-dark"
+                            >
+                              <FontAwesomeIcon icon={faPlay} />
+                            </Link>
+                          </Tooltip>
+                        )}
+                        <Tooltip title="View My Answers">
+                          <Link
+                            href={route("students.quizzes.view.answers", [
+                              quiz.id,
+                            ])}
+                            className="btn btn-sm btn-dark"
+                          >
+                            <FontAwesomeIcon icon={faEye} />
+                          </Link>
                         </Tooltip>
-                      )}
-                      <Tooltip title='View My Answers'>
-                        <Link href={route('students.quizzes.view.answers', [quiz.id])} className='btn btn-sm btn-dark'><FontAwesomeIcon icon={faEye} /></Link>
-                      </Tooltip>
-                    </div>
-                  </td>
-                </tr>
-              )
-            })}
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         ) : (
-          <NotFoundAlert text='No quizzes for this selected subject!' />
+          <NotFoundAlert text="No quizzes for this selected subject!" />
         )}
       </div>
-
     </StudentLayout>
-  )
-}
+  );
+};
 
-export default ListQuizzes
+export default ListQuizzes;
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,77 +1,101 @@
-import '@/assets/css/home.scss'
-import '@/assets/css/landing.scss'
+import "@/assets/css/home.scss";
+import "@/assets/css/landing.scss";
 
-import WelcomeImage from '../assets/images/bg-home.svg'
-import Feature01 from '@/assets/images/student.svg'
-import Feature02 from '@/assets/images/activity.png'
-import Feature03 from '@/assets/images/quiz.png'
-import Feature04 from '@/assets/images/answer.svg'
+import WelcomeImage from "../assets/images/bg-home.svg";
+import Feature01 from "@/assets/images/student.svg";
+import Feature02 from "@/assets/images/activity.png";
+import Feature03 from "@/assets/images/quiz.png";
+import Feature04 from "@/assets/images/answer.svg";
 
 import { Head, Link, usePage } from "@inertiajs/inertia-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane, faSignIn, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPaperPlane,
+  faSignIn,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Welcome = () => {
-
-  const { auth } = usePage().props
+  const { auth } = usePage().props;
 
   return (
     <div className="welcome-page-container">
-      <Head title='o-Edu for universities' />
+      <Head title="o-Edu for universities" />
       <div className="header">
-        <h4><Link>o-Edu</Link></h4>
+        <h4>
+          <Link>o-Edu</Link>
+        </h4>
         <div className="links">
-          {auth ? (
-            <Link href={route('students.dashboard')}><FontAwesomeIcon icon={faUser} /> @{auth.user.username}</Link>
+          {auth.user ? (
+            <Link href={route("students.dashboard")}>
+              <FontAwesomeIcon icon={faUser} /> @{auth.user.username}
+            </Link>
           ) : (
             <>
-              <Link href={route('login')}><FontAwesomeIcon icon={faSignIn} /> Login</Link>
-              <a href='https://www.linkedin.com/in/abdulrahman-saber/' target='_blank'><FontAwesomeIcon icon={faPaperPlane} /> Contact</a>
+              <Link href={route("login")}>
+                <FontAwesomeIcon icon={faSignIn} /> Login
+              </Link>
+              <a
+                href="https://www.linkedin.com/in/abdulrahman-saber/"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faPaperPlane} /> Contact
+              </a>
             </>
           )}
         </div>
       </div>
 
       <div className="page-content">
-
         <div className="text-desc">
-
-          <h5>Welcome to o-Edu which stands for <b>Online Education</b> for University Students!</h5>
-          <p>This education was created for universities to handle everything about students and their semesters!</p>
+          <h5>
+            Welcome to o-Edu which stands for <b>Online Education</b> for
+            University Students!
+          </h5>
+          <p>
+            This education was created for universities to handle everything
+            about students and their semesters!
+          </p>
 
           <div className="features">
             <div className="feature">
-              <img src={Feature01} alt=""/>
+              <img src={Feature01} alt="" />
               <h6>Student Problems Solved</h6>
             </div>
             <div className="feature">
-              <img src={Feature02} alt=""/>
+              <img src={Feature02} alt="" />
               <h6>Friendly UI</h6>
             </div>
             <div className="feature">
-              <img src={Feature03} alt=""/>
+              <img src={Feature03} alt="" />
               <h6>Quiz handler</h6>
             </div>
             <div className="feature">
-              <img src={Feature04} alt=""/>
+              <img src={Feature04} alt="" />
               <h6>High Performance</h6>
             </div>
           </div>
 
           <div className="page-footer">
             {!auth && (
-              <Link href={route('login')} className='btn btn-primary'>Login</Link>
+              <Link href={route("login")} className="btn btn-primary">
+                Login
+              </Link>
             )}
-            <a className='btn btn-dark' href='https://www.linkedin.com/in/abdulrahman-saber/' target='_blank'>Contact us</a>
+            <a
+              className="btn btn-dark"
+              href="https://www.linkedin.com/in/abdulrahman-saber/"
+              target="_blank"
+            >
+              Contact us
+            </a>
           </div>
         </div>
 
-        <img className={'main-l'} src={WelcomeImage} alt=""/>
-
+        <img className={"main-l"} src={WelcomeImage} alt="" />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
